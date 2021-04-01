1d49c5d0fc98edafe5d7b268eeb12e757e9fddbb
AspireOne <matejpesl1@gmail.com>
2021-04-01 11:48:38 +0200

    fix wrong ids

--- app/src/main/java/com/gmail/matejpesl1/mimi/BootReceiver.java
+++ app/src/main/java/com/gmail/matejpesl1/mimi/BootReceiver.java
@@ -11,7 +11,6 @@ public class BootReceiver extends BroadcastReceiver {
 
     // The alarm is unregistered on reboot, so we re-register it on boot (if it should
     // be registered).
-    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
         if (UpdateServiceAlarmManager.isRegistered(context))
