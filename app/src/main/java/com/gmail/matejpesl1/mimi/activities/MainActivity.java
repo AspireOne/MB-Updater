561b25c851323c4e676d7c7c51c81d779712e7f5
AspireOne <matejpesl1@gmail.com>
2021-04-12 12:10:26 +0200

    refactor

--- app/src/main/java/com/gmail/matejpesl1/mimi/activities/MainActivity.java
+++ app/src/main/java/com/gmail/matejpesl1/mimi/activities/MainActivity.java
@@ -98,8 +98,6 @@ public class MainActivity extends AppCompatActivity {
                 RootUtils.askForRoot();
             }).start();
         }
-
-        //updateView();
     }
 
     @Override
