c3bb81b10aed9aed494f073a103db5da83462724
Matěj Pešl <matejpesl1@gmail.com>
2021-06-03 20:22:06 +0200

    tweak internet asserting algorithm
    Remove disabling wifi before enabling data, make connection check duration shorter

--- app/src/main/java/com/gmail/matejpesl1/mimi/utils/InternetUtils.java
+++ app/src/main/java/com/gmail/matejpesl1/mimi/utils/InternetUtils.java
@@ -14,7 +14,7 @@ import java.net.InetAddress;
 public class InternetUtils {
     private static final String TAG = InternetUtils.class.getSimpleName();
     private static final int internetAssertionPingingFreqSecs = 5;
-    private static final int internetAssertionPingingDurationSecs = 60;
+    private static final int internetAssertionPingingDurationSecs = 50;
     public enum DataState { UNKNOWN, ENABLED, DISABLED }
 
     public static DataState getMobileDataState() {
@@ -43,7 +43,7 @@ public class InternetUtils {
 
     public static boolean waitForConnection(int durationSecs, int frequencySecs) {
         int iterations = durationSecs/frequencySecs;
-        int delayMs = (frequencySecs * 1000) - 100; //The check itself takes some time too, so subtract it.
+        int delayMs = (frequencySecs * 1000) - 200; //The check itself takes some time too, so subtract it.
         for (byte i = 0; i < iterations; ++i) {
             SystemClock.sleep(delayMs);
             if (isConnectionAvailable())
@@ -73,9 +73,15 @@ public class InternetUtils {
     }
     public static void setWifiEnabled(Context context, boolean enabled) {
         ((WifiManager)context.getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(enabled);
+        // Workaround for some devices not turning it on unless the screen is on.
+        if (enabled)
+            RootUtils.runCommandAsSu("input keyevent KEYCODE_WAKEUP");
     }
     public static void setDataEnabled(boolean enabled) {
         RootUtils.setMobileDataConnection(enabled);
+        // Workaround for some devices not turning it on unless the screen is on.
+        if (enabled)
+            RootUtils.runCommandAsSu("input keyevent KEYCODE_WAKEUP");
     }
 
     public static void revertToInitialState(Context context, DataState prevDataState, boolean prevWifiEnabled) {
@@ -97,33 +103,19 @@ public class InternetUtils {
         if (isConnectionAvailable())
             return true;
 
-        if (allowWifiChange) {
+        if (allowWifiChange && !initialWifiEnabled) {
             // If connection is not available and the WIFI is off, turn it on
             // and return true if connection is now available.
-            WifiManager wManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+            WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
 
-            // A workaround for wifi not connecting on some systems unless the screen is turned on.
-            RootUtils.runCommandAsSu("input keyevent KEYCODE_WAKEUP");
+            boolean enableSucceeded = wifiManager.setWifiEnabled(true);
+            Log.i(TAG, "Wifi enable succeeded: " + enableSucceeded);
 
-            if (!initialWifiEnabled) {
-                boolean enableSucceeded = wManager.setWifiEnabled(true);
-                Log.i(TAG, "Wifi enable succeeded: " + enableSucceeded);
+            if (enableSucceeded && waitForConnection(internetAssertionPingingDurationSecs, internetAssertionPingingFreqSecs))
+                return true;
 
-                if (enableSucceeded && waitForConnection(internetAssertionPingingDurationSecs, internetAssertionPingingFreqSecs))
-                    return true;
-            }
-
-            // If connection is not available even when the WIFI is on, turn it off
-            // (so that it doesn't interfere with mobile data).
-            boolean disableSucceeded = wManager.setWifiEnabled(false);
+            boolean disableSucceeded = wifiManager.setWifiEnabled(false);
             Log.i(TAG, "Wifi disable succeeded: " + disableSucceeded);
-
-            // If data were already enabled (or unknown) and wifi too, check if the
-            // data will work now without the wifi interfering and return it.
-            // If data were already enabled (or unknown) but wifi not, return false - we can't
-            // do anything else.
-            if (initialDataState == InternetUtils.DataState.ENABLED || initialDataState == InternetUtils.DataState.UNKNOWN)
-                return (initialWifiEnabled && disableSucceeded) && waitForConnection(internetAssertionPingingDurationSecs, internetAssertionPingingFreqSecs);
         }
 
         if (allowDataChange) {
@@ -133,10 +125,16 @@ public class InternetUtils {
                 return false;
 
             // Enable data as a last try and return if the connection is available now.
-            boolean setSucceeded = RootUtils.setMobileDataConnection(true);
-            Log.i(TAG, "Data change allowed. Change succeeded: " + setSucceeded);
-            if (setSucceeded)
-                return waitForConnection(internetAssertionPingingDurationSecs, internetAssertionPingingFreqSecs);
+            boolean onSucceeded = RootUtils.setMobileDataConnection(true);
+            Log.i(TAG, "Data change allowed. Change succeeded: " + onSucceeded);
+
+            // If data enable succeeded and connection is available, return true.
+            if (onSucceeded && waitForConnection(internetAssertionPingingDurationSecs, internetAssertionPingingFreqSecs))
+                return true;
+
+            // Else turn the data back off (initial state).
+            boolean offSucceeded = RootUtils.setMobileDataConnection(false);
+            Log.i(TAG, "Data turn off succeeded: " + offSucceeded);
         }
 
         Log.i(TAG, "Could not establish internet connection.");
